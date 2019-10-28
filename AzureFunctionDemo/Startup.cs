c8c7b93e9b00be5a04951c662a9c04bb0fc9cf7c
stogad <dstoga@pgs-soft.com>
2019-10-28 09:41:27 +0100

    add  Microsoft.Azure.Functions.Extensions and Microsoft.Azure.Functions.Extensions.DependencyInjection; packages

---
+++ AzureFunctionDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+namespace AzureFunctionDemo
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
